7e306db56fa27e8599b500aeb3c22192112ab2fa
Ivan <ivang12@g.ucla.edu>
2021-11-21 18:26:34 -0800

    connect to mongodb

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express') // Import express 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config(); 
+
+//require('dotenv').config()
+
+
+
 const app = express() 
-
-require('dotenv').config()
-
 app.get('/', (req, res) => {
     res.send("Hello World");
 }); 
@@ -12,3 +17,17 @@ const portAccess = process.env.PORT || 8000
 app.listen(portAccess, () => {
     console.log(`Server on port ${portAccess}`);
 })
+
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true}
+  )
+  .then(() => console.log('DB Connected'))
+
+
+  
+
+
+  mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+  });
