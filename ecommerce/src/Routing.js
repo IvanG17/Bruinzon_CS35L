63e3b284841770addd84fb2a2d9d32e54d07280d
David Xiong <xiongdsat@gmail.com>
2021-11-25 02:07:47 -0800

    Added basic menu layout

--- ecommerce/src/Routing.js
+++ ecommerce/src/Routing.js
@@ -3,10 +3,12 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import UserSignUp from './UserComponent/UserSignUp'
 import UserSignIn from './UserComponent/UserSignIn'
 import HomePage from './CoreComponent/HomePage'
+import MenuPage from './CoreComponent/MenuPage'
 
 const Routing  = () => {
     return (<div>
         <Router>
+            <MenuPage/>
             <Routes>
                 <Route path="/" exact element={<HomePage/>}/>
                 <Route path="/signin" exact element={<UserSignIn/>}/>
