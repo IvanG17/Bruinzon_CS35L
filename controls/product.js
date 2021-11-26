ca7dffb1b7631c97b7af1a08299d3d1dcdbab60f
Ivan <ivang12@g.ucla.edu>
2021-11-26 15:01:54 -0800

    save new product creation

--- controls/product.js
+++ controls/product.js
@@ -1,1 +1,18 @@
-const user = require('../design/userdata');
+const product = require('../design/product');
+const { errorHandler } = require('../design/error_handling')
+
+
+exports.create = (req, res) => {
+    const prod = new product(req.body)
+    prod.save((err, name) =>{
+        if (err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+
+
+        }
+        res.json({name});
+
+    })
+}
