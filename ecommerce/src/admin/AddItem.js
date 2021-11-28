d46cea22c519cb6ad0575389a3a523bb30f813d6
skematt <matthewyang2002@hotmail.com>
2021-11-28 01:52:48 -0800

    Created Add Product Feature

--- ecommerce/src/admin/AddItem.js
+++ ecommerce/src/admin/AddItem.js
@@ -4,9 +4,124 @@ import {isAuthenticated} from '../Auth'
 import {Link} from "react-router-dom"
 import {createItem} from './backend'
 
-const addItem = () => {
+const AddItem = () => {
 
-    const {user, token} = isAuthenticated()
+    const {user, token} = isAuthenticated();
+    const [values, setValues] = useState ({
+        name: '',
+        description: '',
+        price: '',
+        products: [],
+        product: '',
+        shipping: '',
+        quantity: '',
+        photo: '',
+        loading: false,
+        error: '',
+        createdProduct: '',
+        redirectToProfile: false,
+        formData: ''
+    });
+
+    const {
+        name,
+        description,
+        price,
+        products,
+        product,
+        shipping,
+        quantity,
+        photo,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData
+    } = values;
+
+    useEffect(() => {
+        setValues({...values, formData: new FormData()})
+    }, [])
+
+    const handleChange = name => event => {
+        const value = name === 'photo' ? event.target.files[0] : event.target.value
+        formData.set(name, value)
+        setValues({...values, [name]: value})
+    }
+
+    const clickSubmit = event => {
+
+    }
+
+    const uploadForm = () => (
+        <form className = "mb-3" onSubmit={clickSubmit}>
+            <h4>Upload photo</h4>
+            <div className = "form-group">
+                <label className = "btn btn-secondary">
+                    <input onChange={handleChange('photo')}
+                           type="file"
+                           name="photo"
+                           accept="image/*"/>
+                </label>
+
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input onChange={handleChange('name')}
+                       type="text"
+                       className="form-control"
+                       value={name} />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Description</label>
+                <textarea onChange={handleChange('description')}
+                       className="form-control"
+                       value={description} />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Price</label>
+                <input onChange={handleChange('price')}
+                       type="number"
+                       className="form-control"
+                       value={price} />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Product</label>
+                <select onChange={handleChange('product')}
+                       className="form-control">
+                    <option value = "to_be_decided">Product A</option>
+                    <option value = "to_be_decided">Product B</option>
+                    <option value = "to_be_decided">Product C</option>
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Shipping</label>
+                <select onChange={handleChange('shipping')}
+                        className="form-control">
+                    <option value = "0">No Shipping</option>
+                    <option value = "1">Yes Shipping</option>
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Quantity</label>
+                <input onChange={handleChange('quantity')}
+                       type="number"
+                       className="form-control"
+                       value={quantity} />
+            </div>
+
+            <button className="btn btn-outline-primary">Create Product</button>
+
+        </form>
+    );
+
+    const {} = values
+
     return (
         <Layout
             title="Add a new item"
@@ -14,9 +129,7 @@ const addItem = () => {
             className="container-fluid">
             <div className="row">
                 <div className = "col-md-8 offset-md-2">
-
-                    ...
-
+                    {uploadForm()}
                 </div>
             </div>
 
@@ -24,4 +137,4 @@ const addItem = () => {
     )
 }
 
-export default addItem
+export default AddItem
