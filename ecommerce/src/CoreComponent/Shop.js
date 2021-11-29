afe18cbc6527f9616e80996423eb95760439e407
Ivan Guan <ivang12@g.ucla.edu>
2021-11-28 16:13:46 -0800

    add components for filtering

--- ecommerce/src/CoreComponent/Shop.js
+++ ecommerce/src/CoreComponent/Shop.js
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from "react";
 import Layout from './Layout'
 import Card from './Card'
 import {getProductTypes} from './ApiCore'
+import Checklist from './Checklist'
+import {prices} from './Prices'
 
 
 
 const ShopPage = () => {
+    const [shopFilters, setFilters] = useState({
+        filters: {product : [], price : []}
+    })
     const [productType, setType] = useState([])
     const [error, setError] = useState(false)
 
@@ -25,6 +30,13 @@ const ShopPage = () => {
 
     }, [])
 
+    const itemFilter  = (filter, filterBy) => {
+        const newFilters = {...shopFilters}
+        newFilters.filters[filterBy] = filter
+        setFilters(newFilters)
+    }
+
+    console.log(productType)
 
     return (
         <Layout 
@@ -34,10 +46,15 @@ const ShopPage = () => {
           >
         <div className ="row">
             <div className = 'col-4'>
-                {JSON.stringify(productType)}
+                <ul>
+                <h4>See specific items with types</h4>
+            
+                <Checklist productType = {productType} itemFilter={ filters =>itemFilter(filters, 'products')}/>
+                
+                </ul>
             </div>
             <div className = 'col-8'>
-                Right Options
+                {JSON.stringify(shopFilters)}
             </div>
 
         </div>
